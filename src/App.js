695685e95ebf39636508b32965e721e5d845cf2a
Gopal Kumar <73684581+gk185062@users.noreply.github.com>
2024-01-03 09:42:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome from inoperable Workflow.
+          Welcome inoperable Workflow.
         </p>
         <a
           className="App-link"
