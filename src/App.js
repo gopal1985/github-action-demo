7fb67ee63e6270265a2612a9ff4c9c41855f51d7
Gopal Kumar <gopal.kumar85@outlook.com>
2024-02-15 13:21:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to another Workflow.
+          Welcome to my first Workflow.
         </p>
         <a
           className="App-link"
